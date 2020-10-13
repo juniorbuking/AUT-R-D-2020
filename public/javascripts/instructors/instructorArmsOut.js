8e69106748cc2bb3d9734cef7d41c4d2583f9932
Callum <callumbradding@gmail.com>
2020-10-13 17:35:07 +1300

    Created an instructor model with arms out using the new instructor clas

---
+++ public/javascripts/instructors/instructorArmsOut.js
@@ -0,0 +1,120 @@
+import { Instructor } from "../instructor.js";
+import { camera } from "../camera.js"
+
+/**
+ * Model for an instructor with their arms stretched out wide and their legs in a natural standing position
+ */
+var instructorArmsOut = new Instructor(
+  // confidence score of the model
+  0.8705385187092949,
+  // keypoints of the model
+  [
+    {
+      score: 0.9992579817771912,
+      part: "nose",
+      position: { x: 302.1460245312819, y: 110.47245731775908 },
+    },
+    {
+      score: 0.9991648197174072,
+      part: "leftEye",
+      position: { x: 311.3038357594603, y: 104.03395305936724 },
+    },
+    {
+      score: 0.9997257590293884,
+      part: "rightEye",
+      position: { x: 293.7380879937523, y: 103.06879797691789 },
+    },
+    {
+      score: 0.822766900062561,
+      part: "leftEar",
+      position: { x: 323.8848252555494, y: 112.99281129894601 },
+    },
+    {
+      score: 0.8192412257194519,
+      part: "rightEar",
+      position: { x: 280.4725040612326, y: 111.43162591116769 },
+    },
+    {
+      score: 0.9883120656013489,
+      part: "leftShoulder",
+      position: { x: 351.84597470151107, y: 158.59872599242917 },
+    },
+    {
+      score: 0.9914131164550781,
+      part: "rightShoulder",
+      position: { x: 251.42136984907643, y: 159.00115535052967 },
+    },
+    {
+      score: 0.9109513163566589,
+      part: "leftElbow",
+      position: { x: 429.7772349006453, y: 184.2622402928005 },
+    },
+    {
+      score: 0.8344789743423462,
+      part: "rightElbow",
+      position: { x: 176.54489674558582, y: 189.24269781745895 },
+    },
+    {
+      score: 0.502980649471283,
+      part: "leftWrist",
+      position: { x: 520.6804333078549, y: 177.99583527163958 },
+    },
+    {
+      score: 0.6388770937919617,
+      part: "rightWrist",
+      position: { x: 75.17451434068276, y: 178.9000221421301 },
+    },
+    {
+      score: 0.9776068925857544,
+      part: "leftHip",
+      position: { x: 341.6981326742191, y: 259.6090224667094 },
+    },
+    {
+      score: 0.975080668926239,
+      part: "rightHip",
+      position: { x: 262.9412693276971, y: 262.48624358378663 },
+    },
+    {
+      score: 0.7642611265182495,
+      part: "leftKnee",
+      position: { x: 336.28789164890463, y: 356.1535909622008 },
+    },
+    {
+      score: 0.9056093096733093,
+      part: "rightKnee",
+      position: { x: 271.84967534163104, y: 353.3028653929651 },
+    },
+    {
+      score: 0.9050049185752869,
+      part: "leftAnkle",
+      position: { x: 337.99611289256535, y: 453.9353303986056 },
+    },
+    {
+      score: 0.7644219994544983,
+      part: "rightAnkle",
+      position: { x: 278.9991854661907, y: 455.69442963935956 },
+    },
+  ],
+  // slopes of the model
+  {
+    leftWrist_leftElbow: 0.06893492342359572,
+    leftElbow_leftShoulder: -0.3293096279310064,
+    leftShoulder_rightShoulder: 0.0040072784821194056,
+    rightShoulder_rightElbow: 0.40388577631228556,
+    rightElbow_rightWrist: -0.1020285751119806,
+    rightShoulder_rightHip: -8.983158961179369,
+    leftShoulder_leftHip: 9.953869621011,
+    leftHip_rightHip: 0.03653295719025469,
+    leftAnkle_leftKnee: -57.24184721345498,
+    leftKnee_leftHip: 17.844781414314134,
+    rightAnkle_rightKnee: -14.321479718542404,
+    rightKnee_rightHip: -10.194486159154536,
+  }
+);
+
+function runModelOnCamera() {
+  var cameraInstructor = new camera();
+  cameraInstructor.loadPosenet(instructorArmsOut);
+}
+
+runModelOnCamera();
