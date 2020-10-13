f29570796af76c7da135760792890373e772c84d
Callum <callumbradding@gmail.com>
2020-10-13 17:32:40 +1300

    Updated image so it takes in a instructor variable

--- public/javascripts/image.js
+++ public/javascripts/image.js
@@ -5,7 +5,6 @@ import {
   renderImageToCanvas,
   toggleInstructor,
 } from "./util.js";
-import { instructor } from "./instructor.js";
 
 const defaultQuantBytes = 2;
 
@@ -19,6 +18,7 @@ const defaultResNetInputResolution = 250;
 
 /**
  * Mobilenet Model
+ * Faster, less accurate pose estimation library
  */
 const model = {
   algorithm: "multi-pose",
@@ -46,6 +46,7 @@ const model = {
 
 /**
  * ResNet Model
+ * Not in use but if wanting a more accurate model then we recommend ResNet
  */
 // const model = {
 //   algorithm: 'multi-pose',
@@ -75,8 +76,12 @@ const model = {
 //   net: null,
 // };
 
+//
 const imageElement = document.getElementById("image");
 
+/**
+ * 
+ */
 posenet
   .load({
     architecture: model.input.architecture,
@@ -96,7 +101,8 @@ posenet
   })
   .then(function (poses) {
     console.log(poses);
-    const canvas = document.getElementById("output");
+
+    const canvas = document.getElementById("outputImg");
 
     renderImageToCanvas(
       imageElement,
@@ -104,14 +110,15 @@ posenet
       canvas
     );
 
-    // draw instructor
+    /* Function used to draw an instructor     
+       NOTE: This function is commented out in order to not draw the instructor model on the image
     toggleInstructor(true);
     drawResults(
       canvas,
       instructor,
       model.multiPoseDetection.minPartConfidence,
       model.multiPoseDetection.minPoseConfidence
-    );
+    ); */
 
     // draw student
     toggleInstructor(false);
@@ -123,23 +130,27 @@ posenet
     );
   });
 
+/**
+ * 
+ * @param {*} canvas 
+ * @param {*} poses 
+ * @param {*} minPartConfidence 
+ * @param {*} minPoseConfidence 
+ */
 function drawResults(canvas, poses, minPartConfidence, minPoseConfidence) {
   const ctx = canvas.getContext("2d");
   poses.forEach((pose) => {
-    // console.log(pose)
     if (pose.score >= minPoseConfidence) {
       if (model.output.showKeypoint) {
-        // console.log("drawkeypoints");
         drawKeypoints(pose.keypoints, minPartConfidence, ctx);
       }
 
       if (model.output.showSkeleton) {
-        // console.log("drawskeleton");
-        drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+        // TODO: fix
+        drawSkeleton(null, pose.keypoints, minPartConfidence, ctx);
       }
 
       if (model.output.showBoundingBox) {
-        // console.log("drawkeyBoundingbox");
         drawBoundingBox(pose.keypoints, ctx);
       }
     }
