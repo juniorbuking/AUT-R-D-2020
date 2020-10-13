0a7aa6a6a4eac8d709181796ee690c29c8158da0
Callum <callumbradding@gmail.com>
2020-10-13 17:33:51 +1300

    Updated utils based on ammended changes

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -1,18 +1,30 @@
-import { instructor } from "./instructor.js";
-
 let isInstructor = true;
 let colour = "aqua";
 const boundingBoxColor = "red";
 const lineWidth = 3;
 
+/**
+ * 
+ * @param {*} c 
+ */
 function setColour(c) {
   colour = c;
 }
 
-function calculateSlope([ay, ax], [by, bx]) {
+/**
+ * 
+ * @param {*} param0 
+ * @param {*} param1 
+ */
+export function calculateSlope([ay, ax], [by, bx]) {
   return (ay - by) / (bx - ax);
 }
 
+/**
+ * 
+ * @param {*} m1 
+ * @param {*} m2 
+ */
 function calculateAngle(m1, m2) {
   // console.log(m1, m2);
   const tan = Math.abs((m2 - m1) / (1 + m1 * m2));
@@ -21,14 +33,30 @@ function calculateAngle(m1, m2) {
   return angle;
 }
 
+/**
+ * 
+ * @param {*} param0 
+ */
 function toTuple({ y, x }) {
   return [y, x];
 }
 
+/**
+ * 
+ * @param {*} t 
+ */
 export function toggleInstructor(t) {
   isInstructor = t;
 }
 
+/**
+ * 
+ * @param {*} ctx 
+ * @param {*} y 
+ * @param {*} x 
+ * @param {*} r 
+ * @param {*} color 
+ */
 export function drawPoint(ctx, y, x, r, color) {
   ctx.beginPath();
   ctx.arc(x, y, r, 0, 2 * Math.PI);
@@ -38,6 +66,11 @@ export function drawPoint(ctx, y, x, r, color) {
 
 /**
  * Draws a line on a canvas, i.e. a joint
+ * @param {*} param0 
+ * @param {*} param1 
+ * @param {*} color 
+ * @param {*} scale 
+ * @param {*} ctx 
  */
 export function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
   ctx.beginPath();
@@ -50,51 +83,57 @@ export function drawSegment([ay, ax], [by, bx], color, scale, ctx) {
 
 /**
  * Draws a pose skeleton by looking up all adjacent keypoints/joints
+ * @param {*} instructor 
+ * @param {*} keypoints 
+ * @param {*} minConfidence 
+ * @param {*} ctx 
+ * @param {*} scale 
  */
-export function drawSkeleton(keypoints, minConfidence, ctx, scale = 1) {
+export function drawSkeleton(instructor, keypoints, minConfidence, ctx, scale = 1) {
   const adjacentKeyPoints = posenet.getAdjacentKeyPoints(
     keypoints,
     minConfidence
   );
 
-  //   console.log("adjacent", adjacentKeyPoints);
-  //   console.log("keypoint", keypoints);
-
-  adjacentKeyPoints.forEach((keypoints) => {
-    if (isInstructor) {
-      setColour("LightGreen");
-    } else {
-      const key1 = `${keypoints[0].part}_${keypoints[1].part}`;
-      const key2 = `${keypoints[1].part}_${keypoints[0].part}`;
-      const instructorSlope =
-        instructor[0].slope[key1] || instructor[0].slope[key2];
-      const studentSlope = calculateSlope(
-        toTuple(keypoints[0].position),
-        toTuple(keypoints[1].position)
-      );
-
-      const angle = Math.floor(calculateAngle(instructorSlope, studentSlope));
-      // console.log(key1, angle);
-
-      if (angle > 8) {
-        setColour("OrangeRed");
-      } else {
+    adjacentKeyPoints.forEach((keypoints) => {
+      if (isInstructor) {
         setColour("Yellow");
+      } else {
+        const key1 = `${keypoints[0].part}_${keypoints[1].part}`;
+        const key2 = `${keypoints[1].part}_${keypoints[0].part}`;
+        const instructorSlope =
+          instructor.slope[key1] || instructor.slope[key2];
+        const studentSlope = calculateSlope(
+          toTuple(keypoints[0].position),
+          toTuple(keypoints[1].position)
+        );
+  
+        const angle = Math.floor(calculateAngle(instructorSlope, studentSlope));
+        // console.log(key1, angle);
+  
+        if (angle > 8) {
+          setColour("Red");
+        } else {
+          setColour("LightGreen");
+        }
       }
-    }
-
-    drawSegment(
-      toTuple(keypoints[0].position),
-      toTuple(keypoints[1].position),
-      colour,
-      scale,
-      ctx
-    );
-  });
+  
+      drawSegment(
+        toTuple(keypoints[0].position),
+        toTuple(keypoints[1].position),
+        colour,
+        scale,
+        ctx
+      );
+    });
 }
 
 /**
- * Draw pose keypoints onto a canvas
+ * Draw keypoints of a pose onto a canvas
+ * @param {*} keypoints 
+ * @param {*} minConfidence 
+ * @param {*} ctx 
+ * @param {*} scale 
  */
 export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
   for (let i = 0; i < keypoints.length; i++) {
@@ -126,6 +165,8 @@ export function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
  * Draw the bounding box of a pose. For example, for a whole person standing
  * in an image, the bounding box will begin at the nose and extend to one of
  * ankles
+ * @param {*} keypoints 
+ * @param {*} ctx 
  */
 export function drawBoundingBox(keypoints, ctx) {
   const boundingBox = posenet.getBoundingBox(keypoints);
@@ -142,7 +183,10 @@ export function drawBoundingBox(keypoints, ctx) {
 }
 
 /**
- * Draw an image on a canvas
+ * Draw an image onto a canvas
+ * @param {*} image 
+ * @param {*} size 
+ * @param {*} canvas 
  */
 export function renderImageToCanvas(image, size, canvas) {
   canvas.width = size[0];
