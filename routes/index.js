b3ead6492847bb2ea3df69ba2dde807697bd57d3
Callum <callumbradding@gmail.com>
2020-10-14 11:12:54 +1300

    Making the home page more pretty

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LES MILLS On Demand Visual Gamification x AUT R&D 2020' });
+  res.render('index', { title: 'LES MILLS On Demand x AUT R&D Visual Gamification Project 2020' });
 });
 
 module.exports = router;
